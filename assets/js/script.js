8dc94f6bbbcdb498c730b127d1f81fb198a06efd
Kat Gomozova <kathy.gomozova@gmail.com>
2020-11-07 13:14:55 -0800

    .

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Frontend JS
+
+var userSearch = $("#exampleInputEmail1");
+
+// Backend JS
+api_key = "6iLNzZCyGjznppIn1mkOPHxYjq1JWAiN"
+url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey"
+
