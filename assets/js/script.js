17b3643ffac0ad5d2d2af2eb4ca2217b45242423
Joshua J. Haller <phone@joshuajhaller.com>
2020-11-07 14:05:24 -0800

    Added style connecting variables.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,11 @@
 
 var userSearch = $("#exampleInputEmail1");
 
-// Backend JS
-api_key = "6iLNzZCyGjznppIn1mkOPHxYjq1JWAiN"
-url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey"
+var searchTermInput = document.getElementById("searchTerm").value;
+var startYearInput = document.getElementById("startYear").value;
+var endYearInput = document.getElementById("endYear").value;
 
+// Backend JS
+api_key = "6iLNzZCyGjznppIn1mkOPHxYjq1JWAiN";
+url =
+  "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey";
